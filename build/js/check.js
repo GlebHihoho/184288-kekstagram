4d19797004f09f9fc7b67145d5d3e49bfb1e7803
Gleb <hihoho@mail.ru>
2016-06-04 10:25:35 +0200

    Work-0.5
    пока бед расчётов над массивами

--- build/js/check.js
+++ build/js/check.js
@@ -1,24 +1,21 @@
-var a;
-var b;
-var sum;
-var square;
-
 function getMessage(a, b) {
-	if (typeof a == "boolean") {
-		if (typeof a == "true") {
-			return("Переданное GIF-изображение анимировано и содержит " + b + " кадров");
+	var sum;
+	var square;
+	if (typeof a === "boolean") {
+		if (typeof a === true) {
+			return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
 		} else {
-			return("Переданное GIF-изображение не анимировано");
+			return "Переданное GIF-изображение не анимировано";
 		}
 	} else {
-		if (typeof a == "number") {
-			return("Переданное SVG-изображение содержит " + a + "объектов и " + b * 4 + "атрибутов");
+		if (typeof a === "number") {
+			return "Переданное SVG-изображение содержит " + a + "объектов и " + b * 4 + "атрибутов";
 		} else {
-			if (typeof a == "object") {
-				return("Количество красных точек во всех строчках изображения:  " + sum);
+			if (Array.isArray(a)) {
+				return "Количество красных точек во всех строчках изображения:  " + sum;
 			} else {
-				if (typeof b == "object") {
-					return("Общая площадь артефактов сжатия: " + square + "  пикселей");
+				if (Array.isArray(b)) {
+					return "Общая площадь артефактов сжатия: " + square + "  пикселей";
 				}
 			}
 		}
