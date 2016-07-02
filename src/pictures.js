091ac5842994bde2b7a63ee52b4c44e43a1bff3a
Gleb <hihoho@mail.ru>
2016-07-02 18:13:53 +0200

    Work-1.8
    изменил загрузку файлов по XMLHttpRequest.

--- src/pictures.js
+++ src/pictures.js
@@ -1,11 +1,13 @@
-/* global pictures */
 'use strict';
 
-var filters = document.querySelector('.filters');
+var pictures = [];
 
+var filters = document.querySelector('.filters');
 filters.classList.add('hidden');
 
 var picturesBlock = document.querySelector('.pictures');
+picturesBlock.classList.add('pictures-loading');
+
 var templateElement = document.getElementById('picture-template');
 var elementToClone;
 
@@ -38,6 +40,42 @@ var getPictureElement = function(data, container) {
   return element;
 };
 
-pictures.forEach(function(picture) {
-  getPictureElement(picture, picturesBlock);
+/** @param {function} callback */
+
+var getPictures = function(callback) {
+  var xhr = new XMLHttpRequest();
+
+  /** @param {ProgressEvent} */
+
+  xhr.onload = function(evt) {
+    var loadedData = JSON.parse(evt.target.response);
+    callback(loadedData);
+    picturesBlock.classList.remove('pictures-loading');
+  };
+  xhr.open('GET', '//o0.github.io/assets/json/pictures.json', true);
+  xhr.send();
+
+  // xhr.timeout = 500;
+  // xhr.ontimeout = function() {
+  //
+  //   picturesBlock.classList.add('pictures-failure');
+  // };
+
+
+  xhr.onerror = function() {
+    picturesBlock.classList.add('pictures-failure');
+  };
+
+};
+
+/** @param {Array.<Object>} pictures */
+var renderPictures = function(picturess) {
+  picturess.forEach(function(picture) {
+    getPictureElement(picture, picturesBlock);
+  });
+};
+
+getPictures(function(loadPictures) {
+  pictures = loadPictures;
+  renderPictures(pictures);
 });
